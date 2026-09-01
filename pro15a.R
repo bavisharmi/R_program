7e8b4d45b7b0dc843c6371a528fac8ed1c17dd39
bavisharmi <bavisharmimk@gmail.com>
2026-09-01 12:54:23 +0530

    Add files via upload

---
+++ pro15a.R
@@ -0,0 +1,11 @@
+transaction=list(
+  c("Milk","Bread","Data"),
+  c("Bread","Butter"),
+  c("Milk","Bread"),
+  c("Milk","Butter"),
+  c("Bread","Butter","Eggs"),
+  c("Milk","Bread","Butter"),
+  c("Milk","Bread","Data"),
+  c("Milk","Bread","Data"),
+  
+)
